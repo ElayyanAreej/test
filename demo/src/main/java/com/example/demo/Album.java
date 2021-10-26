6fab9e6ff585c25b1ba77ceff37572cb7bfd66b1
ElayyanAreej <areej.hossein@gmail.com>
2021-10-26 13:50:45 +0300

    finished lab 11

---
+++ demo/src/main/java/com/example/demo/Album.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+
+public class Album {
+
+    //An Album has a title, an artist, a songCount, a length (in seconds),
+    // and an imageUrl that is a link to that album’s art.
+     private String title;
+     private String artist;
+     private int songCount;
+     private int length;
+     private String imageUrl;
+
+    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String toString(){
+        String album="the artist ";
+        album+=this.artist+" has "+this.title+" album with "+this.songCount+" Songs";
+        return album;
+    }
+}
--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -3,13 +3,29 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//@RequestMapping("/home")
 public class HelloController {
+
+
     @GetMapping("/hello")
     @ResponseBody
     public String helloH() {
         return ("hello world");
     }
+
+    @GetMapping("/capitalize/{text}")
+    public String capitalize(@PathVariable String text){
+        return "capitalize";
+    }
+
+    @GetMapping("/")
+    public String splashPage() {
+        return "splashPage";
+    }
+
 }
